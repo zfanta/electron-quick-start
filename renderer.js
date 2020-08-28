e0307f8d6a3e3c866c29c65934052d40de5c56b9
zfanta <jht008@gmail.com>
2020-08-29 00:59:31 +0900

    Sequelize works

--- renderer.js
+++ renderer.js
@@ -4,3 +4,22 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: `./${(new Date()).getTime()}.sqlite`
+})
+
+sequelize.define('User', {
+  firstName: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.DataTypes.STRING
+  }
+});
+
+sequelize.sync()
