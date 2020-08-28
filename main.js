5eccb1cd0f48175dc0f88c448c9d023eb26fbc01
zfanta <jht008@gmail.com>
2020-08-29 03:00:49 +0900

    webpack does not work

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
   mainWindow.loadURL('http://localhost:8080')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
